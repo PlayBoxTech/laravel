d536c25df971c9b3ef88e6c06d4cfe8e67cee74f
design@playbox.tech <vo@andrew.playbox.tech>
2023-05-26 12:15:51 -0500

    [lesson-34] completed lesson 34

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -16,7 +16,9 @@
 
             <div x-data="{ show: false }" @click.away="show = false">
                 <button @click="show = !show" class="py-2 pl-3 pr-9 text sm font-semibold w-32 text-left inline-flex">
-                    Categories
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+                        
+                    
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                         height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
@@ -30,10 +32,17 @@
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl w-full z-50"
                     style="display: none">
-                    @foreach ($categories as $category)
+                    
                     <a class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white"
+                    href="/">All</a>
+
+                    @foreach ($categories as $category)
+                    <a class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white
+                    {{ isset($currentCategory) && $currentCategory->id == $category->id ? 'bg-blue-500 text-white': ''}}
+                    "
                         href="/categories/{{ $category->slug }}">{{ ucwords($category->name )}}</a>
                     @endforeach
+                    
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,8 @@ Route::get('/', function () {
     $posts = Post::latest()->with('category', 'author')->get();
     //dd ($posts);
     return view('posts', [
-        'posts' => $posts
+        'posts' => $posts,
+        'categories' => Category::all()
     ]);
 });
 
@@ -34,19 +35,22 @@ Route::get('posts/{post:slug}', function (Post $post) {
     //dd ($id);
     
    return view('post', [
-    'post' => $post
+    'post' => $post    
    ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
 
