1232aeac28ffc587eef846333a2c71ac20c7ad7c
Chris Moody <design@playbox.tech>
2023-06-10 20:06:36 -0500

    lesson-40 complete

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,7 @@ Route::get('posts/{post:slug}', [PostController::class,'show']);
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author']),
-        'categories' => Category::all()
+        'posts' => $author->posts
     ]);
 });
 
