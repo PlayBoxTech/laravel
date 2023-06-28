688fcb2a2aab8555b85a87d475eee53393dc20e0
Chris Moody <design@playbox.tech>
2023-06-28 17:34:13 -0500

    [lesson-50] complete

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,29 @@ namespace App\Http\Controllers;
 
 class SessionsController extends Controller
 {
+    
+    public function create()
+    {
+        return view('sessions.create'); 
+    }
+    
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        return back()
+            ->withInput()
+            ->withErrors(['email' => 'Your provided creditials could not be verified']);
+    }
+
     public function destroy()
     {
         auth()->logout();
