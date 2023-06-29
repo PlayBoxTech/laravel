ea2fe07e95a780c868af1d29d4fe3d1435b0dbb1
Chris Moody <design@playbox.tech>
2023-06-28 20:17:06 -0500

    [lesson-51] complete

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,14 +17,14 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (auth()->attempt($attributes)) {
-            session()->regenerate();
-            return redirect('/')->with('success', 'Welcome Back!');
-        }
-
-        return back()
+        if (! auth()->attempt($attributes)) {
+            return back()
             ->withInput()
             ->withErrors(['email' => 'Your provided creditials could not be verified']);
+        }
+        session()->regenerate();
+        return redirect('/')->with('success', 'Welcome Back!');
+        
     }
 
     public function destroy()
