0b926f314bc71fbf65de54d7fc0f5b2d0836455f
Chris Moody <design@playbox.tech>
2023-07-01 23:02:18 -0500

    [lesson-60] Complete.

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use App\Services\Newsletter;
+
+
+class NewsletterController extends Controller
+{
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email'=> 'required|email']);
+        try {
+            
+            $newsletter->subscribe(request('email'));
+        
+      
+        } catch (\Exception $e) {
+           throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our Newsletter List.'
+           ]);
+        }
+        
+        
+        return redirect('/')->with('success', 'You are now Subscribed!'); 
+    
+    }
+}
